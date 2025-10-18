f883918c49c6625444eaa7ef094012ec66d2b39b
Alan Chan <achan961117@gmail.com>
2025-10-18 23:07:36 +0200

    Parse GEO item summary results

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+"""
+Summary of https://www.ncbi.nlm.nih.gov/geo/info/overview.html on GEO concepts
+
+GPLxxx is a _platform_ type
+GSMxxx is a sequencing _sample_, done on a particular platform
+GSExxx is a _series_ of samples
+GDSxxx is a _dataset_ of curated collection of comparable samples on GEO
+
+Specific platforms
+GPL13534: HumanMethylation450
+GPL16304: HumanMethylation450 BeadChip
+GPL21145: MethylationEPIC
+"""
+
+from pprint import pprint
+
 import httpx
 import xmltodict
 
-def main():
-    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+
+E_UTILS_BASE = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
+
+
+def geo_list_studies():
+    url = E_UTILS_BASE + "/esearch.fcgi"
     params = {
         "db": "gds",
         "term": "(GPL13534[Platform] OR GPL21145[Platform] OR GPL16304[Platform]) AND idat[suppFile]",
         "retMax": 5000,
     }
-
     res = httpx.get(url, params=params)
 
     # Check if the request was successful
@@ -22,16 +41,61 @@ def main():
     if 'eSearchResult' not in data:
         raise Exception(f"Data seems malformed. {data}")
 
-    result = data['eSearchResult']
-    count = result.get('Count')
-    id_list = result.get('IdList', {}).get('Id', [])
+    return data['eSearchResult']
 
-    print(f"Query total count: {count}")
-    print(f"Number of IDs returned: {len(id_list)}")
+
+def geo_get_study(id):
+    url = E_UTILS_BASE + "/esummary.fcgi"
+    params = {
+        "db": "gds",
+        "id": id,
+    }
+    res = httpx.get(url, params=params)
+
+    if res.status_code != 200:
+        raise Exception(f"Request failed with status code: {res.status_code}")
+
+    data = xmltodict.parse(res.text)
+    return data['eSummaryResult']
+
+
+def parse_field_value(field):
+    ftype = field['@Type']
+
+    if ftype == 'List':
+        return [parse_field_value(node) for node in field.get('Item', [])]
+    elif ftype == 'Structure':
+        return {
+            node['@Name']: parse_field_value(node)
+            for node in field.get('Item', [])
+        }
+    elif ftype == 'String':
+        return field.get('#text', '')
+    elif ftype == 'Integer':
+        if (x := field.get('#text')) is not None:
+            return x
+        else:
+            return None
+
+
+def parse_summary_item(item):
+    return {
+        field['@Name']: parse_field_value(field)
+        for field in item
+    }
 
 
 if __name__ == "__main__":
     try:
-        main()
+        result = geo_list_studies()
+        count = result.get('Count')
+        id_list = result.get('IdList', {}).get('Id', [])
+
+        print(f"Query total count: {count}")
+        print(f"Number of IDs returned: {len(id_list)}")
+
+        print(f"First ID {id_list[0]}")
+        pprint(parse_summary_item(geo_get_study(id_list[0])['DocSum']['Item']))
+
     except Exception as e:
-        print(f"Program errored {e}")
+        raise
